881f3885edbe0520e80fc260107a71d30554aabb
songhaoreact <15376364250@163.com>
2017-10-12 14:06:55 +0800

    icon docs

--- examples/demos/icon.vue
+++ examples/demos/icon.vue
@@ -3,32 +3,37 @@
         <ic-flexbox row="50" col="10">
             <ic-flexbox-item vertical>
                 <ic-icon name="serach"></ic-icon>
-                <h1>123</h1>
+                <p>serach</p>
             </ic-flexbox-item>
-            <ic-flexbox-item>
-                <ic-icon name="serach"></ic-icon>
+            <ic-flexbox-item vertical>
+                <ic-icon name="del"></ic-icon>
+                <p>del</p>
             </ic-flexbox-item>
-            <ic-flexbox-item>
-                <ic-icon name="serach"></ic-icon>
+            <ic-flexbox-item vertical>
+               <ic-icon name="back"></ic-icon>
+                <p>back</p>
             </ic-flexbox-item>
-            <ic-flexbox-item>
-                <ic-icon name="serach"></ic-icon>
+            <ic-flexbox-item vertical>
+                <ic-icon name="check"></ic-icon>
+                <p>check</p>
             </ic-flexbox-item>
         </ic-flexbox>
         <ic-flexbox row="50" col="10">
-            <ic-flexbox-item>
-                <ic-icon name="serach"></ic-icon>
+            <ic-flexbox-item vertical>
+               <ic-icon name="ellipsis"></ic-icon>
+                <p>ellipsis</p>
             </ic-flexbox-item>
-            <ic-flexbox-item>
-                <ic-icon name="serach"></ic-icon>
+            <ic-flexbox-item vertical>
+               <ic-icon name="checked"></ic-icon>
+                <p>checked</p>
             </ic-flexbox-item>
-            <ic-flexbox-item>
-                <ic-icon name="serach"></ic-icon>
-            </ic-flexbox-item>
-            <ic-flexbox-item>
-                <ic-icon name="serach"></ic-icon>
             </ic-flexbox-item>
         </ic-flexbox>
     </div>
     
 </template>
+<style>
+    p{
+        margin-top:20px;
+    }
+</style>
--- examples/docs/icon.md
+++ examples/docs/icon.md
@@ -1,1 +1,26 @@
-1231231
+## Icon 标题
+
+### 使用指南
+``` javascript
+import { Icon } from 'icui';
+
+Vue.component(Icon.name, Icon);
+```
+
+### 代码演示
+
+#### 
+
+:::demo
+```html
+<ic-icon name="del"></ic-icon>
+```
+:::
+
+### Options
+
+| 参数       | 说明      | 类型       | 默认值       |
+|-----------|-----------|-----------|-------------|
+| name | icon的name | `String` | ``|
+
+
