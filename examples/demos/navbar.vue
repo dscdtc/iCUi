51becb7b495ba9d0c5bf0f616c3a466dc17eaf14
songhaoreact <15376364250@163.com>
2017-10-12 09:54:19 +0800

    change

--- examples/demos/navbar.vue
+++ examples/demos/navbar.vue
@@ -1,21 +1,4 @@
 <template>
-<<<<<<< HEAD
-    <div>
-        <ic-flexbox>
-            <ic-flexbox-item>
-                导航栏
-            </ic-flexbox-item>
-        </ic-flexbox>
-        <ic-navbar
-            title="NavBar"
-            leftContent="返回"
-            rightContent="说明"
-            leftIcon
-            rightIcon
-        ></ic-navbar>
-    </div>
-</template>
-=======
 	<div>
 		<ic-flexbox>
 			<ic-flexbox-item>
@@ -29,9 +12,8 @@
 	</div>
 </template>
 
-<<style>
+<style>
   .title {
     font-size: 20px;
   }
 </style>
->>>>>>> 50a6cc80e0ae8ce81aa28e851511e646c2cb320f
